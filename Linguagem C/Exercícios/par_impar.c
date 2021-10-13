867f73b5175d60b289d1c88467a981a957739695
Fernando H <fhideki_sato@hotmail.com>
2021-10-13 16:09:43 -0300

    par impar

---
+++ Linguagem C/Exercícios/par_impar.c
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cout << "Digite um numero: ";
+    cin >> num;
+
+    if (num%2==0)
+        cout <<"o numero eh par"<<endl;
+    else
+        cout <<"o numero eh impar"<<endl;
+    return 0;
+}
